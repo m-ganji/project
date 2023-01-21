780954ac7ffaffb8c9d91700b40baa6a2665e9ad
Shiva <shivasanaee7697@gmail.com>
2023-01-21 15:40:30 +0330

    request is being sent

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import logo from "../assets/images/logo_login.png"
 import ReCAPTCHA from "react-google-recaptcha";
+import axios from 'axios';
+
 
 function onChange(value) {
   console.log("Captcha value:", value);
 }
 
+var data = 'username=postgres&password=pass&grecaptcha=03AD1IbLDcPDi9-5saTzc719OitCXzUAyM9M74Y5vDwwlC9qgeRSr5IdWYeVT9VFCX7U99Xe8vqYJUTH-Cjdbcr5yTF9KvxrcKZ4e5Q8idtEqNJAanamO6xEqpTYQNc0AiptytSN2XOFvcTl0wYbHeiovkawv8tjrs_pAn4cQIUX3skdrguDJP1-WBLTRImbGAMowmj_nsEkOnRdjjJ5nU9YY33xgD7rFXFxosDKensny_am8gcJXY3SrNZY_PZzQpv53dwg2uIYfAv0vW84n4AUJXRElRW0zU7SsxEU-44x20zwgLdLIRq79lF1Z_8Gvx3R8zpWlSdHWPeXbgj1px0QJ3XYvNo10IiMExCsD1MyJb9rCCTeRU_oOEFrHXoo4TUQdEaoDVLp0C5B4A6bGnJ6myIXW3zgYj1MclGtgyJLndzAZzV0M-k16d8yDj6JKiKnLdW6ehCIBincU6U11q-oLkyY1kQP-SrrTaP44nj2SY5AUzrexg0a5SIs7b_UwzY-0sKcmw6Z8ozDQeRJHCpNozz9_DTNvSsbHTOCeI-eLUPNhJbl-8w1g';
+
+var config = {
+  method: 'post',
+  url: 'https://rassam-pars.ir/rassam/api/public/session',
+  data: data
+};
+
 export default function Login() {
   return (
     <div className='d-flex justify-content-center align-items-center w-100 vh-100 col-form-label background-login'>
       <div className='w-50 d-flex flex-row bg-white container-login border-1 rounded responsive-form'>
-        <form action="?" method="post" autocomplete="off" className='w-50 d-flex justify-content-center align-items-center flex-column'>
+        <div className='w-50 d-flex justify-content-center align-items-center flex-column'>
           <p className='w-75 mt-5 d-flex mb-5 textbelow' dir='rtl'>ورود کاربر</p>
           <input name="username" className="w-75 mb-1 input-height border rounded plchldr" dir='rtl' type="text" id="username" placeholder="نام کاربری" autocomplete="off" height={200} />
           <input name="password" className="w-75 mb-3 input-height border rounded plchldr" dir='rtl' type="password" id="password" placeholder="کلمه عبور" autocomplete="off" />
@@ -21,8 +31,20 @@ export default function Login() {
             // className="mb-2 g-recaptcha w-100"
             />
           </div>
-          <button className='border-0 w-75 mb-3 pt-1 pb-1 yellow rounded textlogin'>ورود به سیستم </button>
-        </form>
+          <button className='border-0 w-75 mb-3 pt-1 pb-1 yellow rounded textlogin' onClick={() => {
+            axios(config)
+              .then(function (response) {
+                console.log(JSON.stringify(response.data));
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+
+            // axios.post("https://rassam-pars.ir/rassam/api/public/session").then((response) => {
+            //   console.log(response)
+            // });
+          }}>ورود به سیستم </button>
+        </div>
         <div className='w-50 yellow d-flex align-items-center justify-content-center flex-column text-black'>
           <img src={logo} className="mt-5 mb-3 w-50" />
           <p className='textbelow'>
@@ -33,6 +55,6 @@ export default function Login() {
           </p>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
