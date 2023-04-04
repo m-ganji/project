5c6e0e0a5cb0d6b5a72301150b9cff37a7ced0ef
Shiva <shivasanaee7697@gmail.com>
2023-04-04 09:30:34 +0430

    popup is somehow ready

--- src/views/Elements/Drawer.js
+++ src/views/Elements/Drawer.js
@@ -5,7 +5,10 @@ import { MdHome } from "react-icons/md";
 import { AiFillSetting } from "react-icons/ai";
 import { handleButtonSituation } from "../../redux/layout";
 import { useDispatch } from "react-redux";
-// import { AiOutlineSearch } from "react-icons/ai";
+import Popup from 'reactjs-popup';
+// import PopupButton from "./PopupButton";
+// import Popup from 'reactjs-popup';
+
 
 export default function Drawer() {
   const dispatch = useDispatch();
@@ -41,12 +44,61 @@ export default function Drawer() {
           </button>
         )}
         {isOpen && (
-          <button
-            className="smallcircle d-flex justify-content-center align-items-center border-0"
-          // onClick={() => setIsOpenHome(!isOpenHome)}
+
+          <Popup
+            trigger={<button
+              className="smallcircle d-flex justify-content-center align-items-center border-0"
+            // onClick={() => setIsOpenHome(!isOpenHome)}
+            >
+              <AiFillSetting size="1.5em" />
+            </button>}
+            modal
+            nested
           >
-            <AiFillSetting size="1.5em" />
-          </button>
+            {close => (
+              <div >
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Modal Title </div>
+                <div className="content">
+                  {' '}
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+                  Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+                  delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+                  <br />
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+                  commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+                  explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                </div>
+                <div className="actions">
+                  <Popup
+                    trigger={<button className="button"> Trigger </button>}
+                    position="top center"
+                    nested
+                  >
+                    <span>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                      magni omnis delectus nemo, maxime molestiae dolorem numquam
+                      mollitia, voluptate ea, accusamus excepturi deleniti ratione
+                      sapiente! Laudantium, aperiam doloribus. Odit, aut.
+                    </span>
+                  </Popup>
+                  <button
+                    className="button"
+                    onClick={() => {
+                      console.log('modal closed ');
+                      close();
+                    }}
+                  >
+                    close modal
+                  </button>
+                </div>
+              </div>
+            )}
+          </Popup>
+
+
         )}
         {/* {isOpen && <button className='smallcircle d-flex justify-content-center align-items-center border-0'><AiOutlineSearch size="1.5em" /></button>} */}
       </div>
